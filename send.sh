22dfc69585e12dac82c6a7a62c4f065603ce510d
Giordano Scalzo <giordano.scalzo@gmail.com>
2012-07-02 16:27:41 +0200

    fix send script

--- send.sh
+++ send.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APPNAME=$1
-CONTENT="{'log': '$2'}"
+CONTENT="{\"log\": \"$2\"}"
 
 echo $CONTENT
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d "$CONTENT" http://$APPNAME.herokuapp.com/log
